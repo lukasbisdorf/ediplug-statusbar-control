22832e9319b25611fcfe7b15c60c5f3f78cdae43
Lukas Bisdorf <info@lukas-bisdorf.de>
2015-12-03 10:42:37 +0100

    Switch works, but hardcoded credentials

--- LightSwitch/AppDelegate.swift
+++ LightSwitch/AppDelegate.swift
@@ -8,20 +8,71 @@
 
 import Cocoa
 
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    
+    @IBOutlet weak var statusMenu: NSMenu!
+    @IBOutlet weak var statusMenuItem: NSMenuItem!
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    let schreibtisch = switchController(username: "admin", password: "stromanaus", host: "10.50.2.34")
+    
+    let SWITCH_ON_STATE_LABEL = "Lampe ausschalten!"
+    let SWITCH_OFF_STATE_LABEL = "Lampe einschalten!"
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let iconOn = NSImage(named: "statusOn")
+        let iconOff = NSImage(named: "statusOff")
+        
+
+        statusItem.image = iconOff
+        statusItem.menu = statusMenu
+        
+        schreibtisch.events.listenTo("off", action: self.statusChanged)
+        schreibtisch.events.listenTo("on", action: self.statusChanged)
+        
+        schreibtisch.getStatus()
+        var timer = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "checkStatus",userInfo: nil, repeats: true)
+        
+    }
+    
+    func checkStatus() {
+        schreibtisch.getStatus()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
+    
+    func statusChanged(information: Any?) {
+        if let info = information as? String {
+            if info == "on" {
+                statusItem.image = NSImage(named: "statusOn")
+                statusMenuItem.title = SWITCH_ON_STATE_LABEL
+            }
+            if info == "off" {
+                statusItem.image = NSImage(named: "statusOff")
+                statusMenuItem.title = SWITCH_OFF_STATE_LABEL
+            }
+        }
+    }
+    
+    @IBAction func menuClicked(sender: NSMenuItem) {
+        
+        if(sender.title == SWITCH_ON_STATE_LABEL) {
+            statusItem.image = NSImage(named: "statusOff")
+            schreibtisch.turnOff()
+            
+        }
+        else {
+            statusItem.image = NSImage(named: "statusOn")
+            schreibtisch.turnOn()
+        }
+        
+    }
 
 }
 
---
+++ LightSwitch/switchController.swift
@@ -0,0 +1,97 @@
+//
+//  switchController.swift
+//  LightSwitch
+//
+//  Created by Lukas Bisdorf on 02.12.15.
+//  Copyright © 2015 Lukas Bisdorf. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class switchController {
+    
+    let FILE_NAME_OF_SWITCH_ON_DOCUMENT: String = "switchOn"
+    let FILE_NAME_OF_SWITCH_OFF_DOCUMENT: String = "switchOff"
+    let FILE_NAME_OF_SWITCH_STATUS_DOCUMENT: String = "switchStatus"
+    
+    let events = EventManager()
+    
+    let username: String
+    let password: String
+    let host: String
+    
+    
+    init(username: String, password: String, host: String) {
+        self.username = username
+        self.password = password
+        self.host = host
+    }
+    
+    private func createBase64String(input: String) -> String {
+        let utf8str = input.dataUsingEncoding(NSUTF8StringEncoding)
+        return utf8str!.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
+    }
+    
+    
+    private func uploadXmlToHost(xml: NSURL) {
+        let headers = [
+            "Authorization": "Basic " + createBase64String(username + ":" + password)
+        ]
+        
+        Alamofire.upload(
+            .POST,
+            "http://" + host + ":10000/smartplug.cgi",
+            headers: headers,
+            multipartFormData: { multipartFormData in
+                multipartFormData.appendBodyPart(fileURL: xml, name: "file")
+            },
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .Success(let upload, _, _):
+                    upload.responseData{ response in
+                        if let value: NSData = response.result.value {
+                            // Decode the Base64 Value.
+                            let post = NSString(data: value, encoding: NSUTF8StringEncoding)
+                            
+                            if (post?.rangeOfString(">OFF<").length != 0) {
+                                self.events.trigger("off", information: "off")
+                                
+                            }
+                            
+                            if (post?.rangeOfString(">ON<").length != 0)  {
+                                self.events.trigger("on", information: "on")
+                            }
+                            if (post?.rangeOfString(">OK<").length != 0)  {
+                                self.getStatus()
+                            }
+                        }
+                    }
+                case .Failure(let encodingError):
+                    print(encodingError)
+                }
+            }
+            
+        )
+    }
+    
+    
+    func turnOn() {
+        let document = NSBundle.mainBundle().URLForResource(FILE_NAME_OF_SWITCH_ON_DOCUMENT, withExtension: "xml")
+        uploadXmlToHost(document!)
+    }
+    
+    func turnOff() {
+        let document = NSBundle.mainBundle().URLForResource(FILE_NAME_OF_SWITCH_OFF_DOCUMENT, withExtension: "xml")
+        uploadXmlToHost(document!)
+    }
+    func getStatus() -> Bool {
+        let document = NSBundle.mainBundle().URLForResource(FILE_NAME_OF_SWITCH_STATUS_DOCUMENT, withExtension: "xml")
+        uploadXmlToHost(document!)
+        return false
+    }
+    
+    
+    
+    
+}
